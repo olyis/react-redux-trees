e7609a1a3810371d83cb59ea6eb1d3331cd91d2c
Oliver Sourbut <olyis@live.co.uk>
2017-03-23 21:21:27 +0000

    augment int reducer with direct set capability (no type checking)

--- src/reducers/int.js
+++ src/reducers/int.js
@@ -1,31 +1,38 @@
 const Action = Object.freeze({
     DECR: -1,
     RESET: 0,
-    INCR: 1
+    INCR: 1,
+    SET: 2
 });
 
 export function decr() {
-    return {type: Action.DECR};
+    return {type: Action.DECR}
 }
 
 export function reset() {
-    return {type: Action.RESET};
+    return {type: Action.RESET}
 }
 
 export function incr() {
-    return {type: Action.INCR};
+    return {type: Action.INCR}
+}
+
+export function set(i) {
+    return {type: Action.SET, value: i}
 }
 
 export function int(state = 0, action) {
     switch(action.type) {
         case Action.DECR:
-            return state - 1;
+            return state - 1
         case Action.RESET:
-            return 0;
+            return 0
         case Action.INCR:
-            return state + 1;
+            return state + 1
+        case Action.SET:
+            return +action.value
         default:
-            return state;
+            return state
     }
 }
 
